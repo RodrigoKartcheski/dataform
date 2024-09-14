fdd2f160fbc208ef8ba8464b97e6fc2241fad8f7
Rodrigo A Kartcheski <rodrigo.kartcheski@gmail.com>
2024-09-14 23:03:25 +0000

    ddd

--- includes/constants.js
+++ includes/constants.js
@@ -1,3 +1,8 @@
   // filename is includes/constants.js
-  const PROJECT_ID = "gcp-composer-433712";
-  module.exports = { PROJECT_ID };
+// filename: includes/constants.js
+const SRC_PROJECT_ID = "gcp-composer-433712";
+const TGT_PROJECT_ID = "compose-dataflow-3065";
+const SRC_DATASET_ID = "mydataset";
+const TGT_DATASET_ID = "mydataset";
+
+module.exports = { SRC_PROJECT_ID, SRC_DATASET_ID, TGT_PROJECT_ID, TGT_DATASET_ID };
