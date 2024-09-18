3c3b9799e99a84771a952ced204ff6bc84c3e0d1
Rodrigo A Kartcheski <rodrigo.kartcheski@gmail.com>
2024-09-18 01:28:42 +0000

    ater

--- definitions/definitions.js
+++ definitions/definitions.js
@@ -5,7 +5,7 @@
   timestamp: "updated_at", // A field that stores a timestamp or date of when the row was last changed.
   source: {
     schema: "dataform",     // The source table to build slowly changing dimensions from.
-    name: "fonte",
+    name: "fonte_copy",
   },
   columns: {coluna1: "coluna1", coluna1: "coluna2", updated_at: "Timestamp for updates"},
   incrementalConfig: {        // Any configuration parameters to apply to the incremental table that will be created.
